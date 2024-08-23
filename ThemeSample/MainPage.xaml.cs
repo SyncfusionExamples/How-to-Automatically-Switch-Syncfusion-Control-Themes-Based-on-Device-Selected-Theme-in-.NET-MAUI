afc72cb1f6d87132e4a3cb348777c681bfa03efd
Brundha <brundha.sf3969@gmail.com>
2024-08-23 10:37:35 +0530

    Removed the unwanted code

--- ThemeSample/MainPage.xaml.cs
+++ ThemeSample/MainPage.xaml.cs
@@ -4,21 +4,9 @@ namespace ThemeSample
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";            
         }
 
         protected override void OnAppearing()
